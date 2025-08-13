2eab2f7226cc55f4afcd5253c499d3e4a3a250a6
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-08-13 17:50:52 +0530

    router setup and working signup route

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"log"
 	"server/db"
+	"server/internal/user"
+	"server/router"
 )
 
 func main() {
-	_, err := db.Newdatabase()
+	dbConn, err := db.Newdatabase()
 	if err != nil {
 		log.Fatalf("could not initialize DB conn. %s", err)
 	}
+
+	userRep := user.NewRepository(dbConn.GetDB())
+	userSrv := user.NewService(userRep)
+	userHandler := user.NewHandler(userSrv)
+
+	router.InitRouter(userHandler)
+	router.Start("0.0.0.0:8080")
 }
